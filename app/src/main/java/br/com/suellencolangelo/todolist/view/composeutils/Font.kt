f34cb452e3f58e44d4db218be1ca70bf7874da15
Suellen Colangelo <suellen.cola@gmail.com>
2021-09-09 22:35:27 -0300

    Adding roboto font to the project and adding design styles

---
+++ app/src/main/java/br/com/suellencolangelo/todolist/view/composeutils/Font.kt
@@ -0,0 +1,15 @@
+package br.com.suellencolangelo.todolist.view.composeutils
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import br.com.suellencolangelo.todolist.R
+
+val robotoFontFamily = FontFamily(
+    Font(R.font.roboto_black, FontWeight.Black),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_thin, FontWeight.Thin),
+)
---
+++ app/src/main/java/br/com/suellencolangelo/todolist/view/composeutils/TextDesignStyle.kt
@@ -0,0 +1,22 @@
+package br.com.suellencolangelo.todolist.view.composeutils
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+object TextDesignStyle {
+    val Header1 =
+        TextStyle(fontFamily = robotoFontFamily, fontWeight = FontWeight.Medium, fontSize = 20.sp)
+    val Header2 =
+        TextStyle(fontFamily = robotoFontFamily, fontWeight = FontWeight.Medium, fontSize = 18.sp)
+    val Header3 =
+        TextStyle(fontFamily = robotoFontFamily, fontWeight = FontWeight.Medium, fontSize = 16.sp)
+    val Header4 =
+        TextStyle(fontFamily = robotoFontFamily, fontWeight = FontWeight.Medium, fontSize = 14.sp)
+    val SubTitle1 =
+        TextStyle(fontFamily = robotoFontFamily, fontWeight = FontWeight.Normal, fontSize = 18.sp)
+    val SubTitle2 =
+        TextStyle(fontFamily = robotoFontFamily, fontWeight = FontWeight.Normal, fontSize = 16.sp)
+    val SubTitle3 =
+        TextStyle(fontFamily = robotoFontFamily, fontWeight = FontWeight.Light, fontSize = 14.sp)
+}
